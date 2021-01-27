c1ca28abf41a1cbd5c211c561df014ad71cfd7ae
Thomas Trarbaugh <thomaswaynet@live.com>
2021-01-27 09:08:14 -0600

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
+    }
+}
